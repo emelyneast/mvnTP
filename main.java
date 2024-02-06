5572eb1ccc797db97d12bad0a0dd1821aa0bb2dd
melyne <alice.mayerma@gmail.com>
2024-02-06 12:15:13 +0100

    fait pas chier

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-package com.example;
+package com.example; // Définit le package dans lequel se trouve la classe Main
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main { // Définit une classe nommée Main
+    public static void main(String[] args) { // La méthode principale de votre programme
+        System.out.println("Hello World!"); // Affiche "Hello World!" dans la console
     }
 }
